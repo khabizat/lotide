365097f29c5dced7cd1bd515f1e21810d13158f9
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-03 02:35:32 +0000

    Function that returns first element in an array

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const head = function(array){
+  return array[0];
+};
+
+assertEqual(head([11,12,14]), 11);
+assertEqual(head(["Kelly", "Pam", "Jim"]), "Kelly");
+assertEqual(head([]), 5); 
