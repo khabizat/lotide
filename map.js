fe0cf21e2cf74772a20eb555a88139118b93df51
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-06 01:29:19 +0000

    Implemented map higher-order function

---
+++ map.js
@@ -0,0 +1,45 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+
+
+//Test Cases
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(map(words, word => word[word.length - 1]), [ 'd', 'l', 'o', 'r', 'm' ]);
+assertArraysEqual(map(words, word => word[0].toUpperCase()), [ 'G', 'C', 'T', 'M', 'T' ]);
+
+
+
+
