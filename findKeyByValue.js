fdb9f5acbcc9fdf735addfd937e69ab6b1a6523f
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-08 16:54:51 +0000

    Function that takes in an object and a value, and returns the first key which contains the given value

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let result = undefined;
+  let objectKeys = Object.keys(object);
+  //let objectValues = Object.values(object);
+  for (let key of objectKeys) {
+    if (object[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+//console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
