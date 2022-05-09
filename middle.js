618f7cd2788de0b95afde7ac3f827de333337d84
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-09 20:17:43 +0000

    Refactored code to use a module

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -32,8 +12,4 @@ const middle = function(array) {
   return result;
 };
 
-assertArraysEqual(middle([1, 2, 7, 4, 5]), [7]);
-assertArraysEqual(middle([1, 2, 7, 4, 5, 8]), [7, 4]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), [])
+module.exports = middle;
