6c2d049eadd36c18688be9e445c5bac9ad312bb1
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-09 19:01:27 +0000

    Refactored and exported the function

--- head.js
+++ head.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
-  }
-};
+
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([11,12,14]), 11);
-assertEqual(head(["Kelly", "Pam", "Jim"]), "Kelly");
-assertEqual(head([]), 5);
-
+module.exports = head;
