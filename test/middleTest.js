de8982440a645d57113896d63f01036537cd5800
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-09 22:02:56 +0000

    Updated to Mocha and Chai based test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,21 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 7, 4, 5]), [7]);
-assertArraysEqual(middle([1, 2, 7, 4, 5, 8]), [7, 4]);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("returns [7] for [1, 2, 7, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 7, 4, 5]),[7]);
+  });
+  it("returns [7, 4] for [1, 2, 7, 4, 5, 8]", () => {
+    assert.deepEqual(middle([1, 2, 7, 4, 5, 8]), [7, 4]);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+});
