b9897cb05692791885e22cb680d0751f9bd93492
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-09 21:52:55 +0000

    Updated to Mocha and Chai based test code

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,16 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const fruits = ['apple', 'orange', 'pear', 'watermelon'];
-tail(fruits);
-assertEqual(fruits.length, 4);
+
+describe("#tail", () => {
+  it("returns ['orange', 'pear', 'watermelon'] for ['apple', 'orange', 'pear', 'watermelon']", () => {
+    assert.deepEqual(tail(['apple', 'orange', 'pear', 'watermelon']),['orange', 'pear', 'watermelon']);
+  });
+  it("returns [] for ['orange']", () => {
+    assert.deepEqual(tail(['orange']), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
